cc04ed00c05b20b9bec1dc2dc1e68d7c7e747d64
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2018-05-06 15:56:28 -0500

    update macros

--- macros/layers.C
+++ macros/layers.C
@@ -9,11 +9,11 @@ const double receiver_depth = -ft_to_mtr;
 
 
 
-void summit13(int depth_in_ft = 200, int firn = 0, double f = 0.35, double w = 0.35, bool movie = true) 
+void layers(int depth_in_ft = 20, int firn = 0, double f = 0.35, double w = 0.2, bool movie = true) 
 {
 
   /* define and make the output dir */ 
-  TString dir; dir.Form("summit13_out/depth_%d_f_%g_w_%g_firn_%d", depth_in_ft,f,w, firn); 
+  TString dir; dir.Form("layers/depth_%d_f_%g_w_%g_firn_%d", depth_in_ft,f,w, firn); 
   TString cmd; cmd.Form("mkdir -p %s", dir.Data()); system(cmd.Data()); 
 
   /* use "arthern model" */
@@ -23,20 +23,20 @@ void summit13(int depth_in_ft = 200, int firn = 0, double f = 0.35, double w = 0
   if (firn == 0) frn = &arthern; 
   if (firn == 1 || firn == 2) 
   {
-
-    double z = -3; 
-    double A = firn == 1 ? 100 : -100; 
-    double sigma = 0.2; 
-    frn = new iceprop::PerturbedFirn(arthern, 1,&z,&A,&sigma); 
+    double z[] = {-3,-6,-9}; 
+    double A1[] = {150,100,50}; 
+    double A2[] = {-150,-100,50} ; 
+    double sigma[] = {0.3,0.2,0.1}; 
+    frn = new iceprop::PerturbedFirn(arthern, 3,z,firn == 1 ?A1 : A2,sigma); 
   }
 
   /* Define simulation geometry */ 
   iceprop::SimGeometry g; 
-  g.max_depth =(TMath::Max(depth_in_ft+100,500) * ft_to_mtr); 
+  g.max_depth =TMath::Max(3*depth_in_ft,30); 
   g.sky_height=20; 
-  g.resolution=10;
-  g.output_skip_factor=5; 
-  g.max_r = 500; 
+  g.output_skip_factor=10; 
+  g.resolution=20; 
+  g.max_r = 100; 
 
 
   /* Define the source */ 
@@ -44,17 +44,6 @@ void summit13(int depth_in_ft = 200, int firn = 0, double f = 0.35, double w = 0
 
   /* Initialize the simulation */ 
   iceprop::Sim sim(frn, &g, &s); 
-
-  /* add time domain measurements */ 
-  sim.addTimeDomainMeasurement( meep::Ez, 1*ft_to_mtr, -depth_in_ft*ft_to_mtr); 
-  sim.addTimeDomainMeasurement( meep::Ez, 200 * ft_to_mtr, receiver_depth); 
-  sim.addTimeDomainMeasurement( meep::Ez, 480 * ft_to_mtr,receiver_depth ); 
-  sim.addTimeDomainMeasurement( meep::Ez, 1000*ft_to_mtr, receiver_depth); 
-  sim.addTimeDomainMeasurement( meep::Ez, 1050*ft_to_mtr, receiver_depth); 
-  sim.addTimeDomainMeasurement( meep::Ez, 50*ft_to_mtr, -depth_in_ft); 
-  sim.addTimeDomainMeasurement( meep::Ez, 500*ft_to_mtr, -depth_in_ft); 
-  sim.addTimeDomainMeasurement( meep::Ez, 1000*ft_to_mtr, -depth_in_ft); 
-  sim.addTimeDomainMeasurement( meep::Ez, 1500*ft_to_mtr, -depth_in_ft); 
 
 
   /* If movie, add step output*/ 
@@ -70,8 +59,8 @@ void summit13(int depth_in_ft = 200, int firn = 0, double f = 0.35, double w = 0
   sim.trackGlobalMaximum(meep::Ez); 
   sim.trackGlobalIntegral(meep::Ez); 
 
-  /* run the simulation for 3.5 us*/ 
-  sim.run(3500); 
+  /* run the simulation for 2000 ns*/ 
+  sim.run(300); 
 
   /* that's it for running the simulation, the rest plots and saves some of the stuff we made*/ 
   
@@ -104,7 +93,6 @@ void summit13(int depth_in_ft = 200, int firn = 0, double f = 0.35, double w = 0
   sim.getIntegrals()[0].integ->Write(); 
   sim.getIntegrals()[0].tfirst->SetDirectory(&fout); 
   sim.getIntegrals()[0].tfirst->Write(); 
- 
   fname.Form("%s/time_graphs.pdf", dir.Data()); 
   ms->SaveAs(fname.Data()); 
 
