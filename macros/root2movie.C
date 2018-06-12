6a0a1a33db0e0860a63c802a825f5ed33c4d2b67
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2018-06-12 10:14:00 -0500

    change units

--- macros/root2movie.C
+++ macros/root2movie.C
@@ -2,7 +2,7 @@
 struct movie_opts
 {
   bool log = true; 
-  double min = 1e-10; 
+  double min = 1e-8; 
   double max = 1; 
   const char * tmpdir = ".movie"; 
   int width = 1920; 
@@ -12,6 +12,33 @@ struct movie_opts
 
 
 }; 
+
+TH2 * toFeet(TH2* h)
+{
+
+  const double meters_to_ft = 3.28084; 
+  TH2 * hfeet = new TH2D(TString::Format("%s_ft",h->GetName()), h->GetTitle(), 
+                         h->GetNbinsX(), h->GetXaxis()->GetXmin() * meters_to_ft, h->GetXaxis()->GetXmax()*meters_to_ft, 
+                         h->GetNbinsY(), h->GetYaxis()->GetXmin() * meters_to_ft, h->GetYaxis()->GetXmax()*meters_to_ft); 
+
+  TString xaxis(h->GetXaxis()->GetTitle()); 
+  xaxis.ReplaceAll("r (m)","d (ft)"); 
+  TString yaxis(h->GetYaxis()->GetTitle()); 
+  yaxis.ReplaceAll("(m)","(ft)"); 
+  hfeet->GetXaxis()->SetTitle(xaxis); 
+  hfeet->GetYaxis()->SetTitle(yaxis); 
+
+
+  for (int i = 1; i < h->GetNbinsX(); i++) 
+  {
+    for (int j = 1; j < h->GetNbinsY(); j++) 
+    {
+      hfeet->SetBinContent(i,j, h->GetBinContent(i,j)); 
+    }
+  }
+
+  return hfeet; 
+}
 
 void root2movie(const char* fname, const char * movie_name = "movie.mp4",
                 const char * tree_name = "Ez_mag", 
@@ -46,28 +73,31 @@ void root2movie(const char* fname, const char * movie_name = "movie.mp4",
   for (int i = 0; i < maxi; i++)
   {
     tree->GetEntry(i); 
+    TH2 * ft = toFeet(hist); 
 
     if (opts.min)
     {
-      hist->SetMinimum(opts.min); 
+      ft->SetMinimum(opts.min); 
     }
     if (opts.max) 
     {
-      hist->SetMaximum(opts.max); 
+      ft->SetMaximum(opts.max); 
     }
 
-    hist->Draw("colz"); 
+    ft->Draw("colz"); 
+    ft->SetStats(false); 
 
     if (opts.line_at_zero) 
     {
-      line.SetPoint(0,hist->GetXaxis()->GetXmin(),0); 
-      line.SetPoint(1,hist->GetXaxis()->GetXmax(),0); 
+      line.SetPoint(0,ft->GetXaxis()->GetXmin(),0); 
+      line.SetPoint(1,ft->GetXaxis()->GetXmax(),0); 
       line.Draw("lsame"); 
     }
 
 
     str.Form("%s/frame_%06d.png", opts.tmpdir, i); 
     c->SaveAs(str.Data()); 
+    delete ft; 
   }
 
   str.Form("ffmpeg -y -i %s/frame_%%06d.png -c:v libx264 -pix_fmt yuv420p %s", opts.tmpdir, movie_name); 
